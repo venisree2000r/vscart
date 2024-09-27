14c10dc99b17730e70d4fbfd727ef5b873a91224
venisree.rn <venisree.rn@pitsolutions.com>
2024-09-27 15:57:46 +0530

    creating folders and init npm

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vscart
 ecommervce using MERN stack
 
-This is my first change 
+This is my first changes
---
+++ backend/server.js
@@ -0,0 +1,4 @@
+const app = require('./app')  // import app obejct from app.js
+const dotenv = require('dotenv')
+
+dotenv.config({path: "config/config.env"})
